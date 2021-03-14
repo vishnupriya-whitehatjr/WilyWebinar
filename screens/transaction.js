ac2b2918b6ef6e8d3b5d801a0fd4d93bd1ad5804
vishnupriya-whitehatjr <vishnupriyayerram28@gmail.com>
2021-03-14 23:26:46 +0530

    commit class 70

--- screens/transaction.js
+++ screens/transaction.js
@@ -44,9 +44,7 @@ export default class Transaction extends Component {
       );
     } else if (this.state.buttonState === "normal") {
       return (
-        <View
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
+        <View style={styles.container}>
           <Text>
             {camPermission === true
               ? this.state.scannedData
@@ -71,3 +69,42 @@ export default class Transaction extends Component {
     }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  displayText: {
+    fontSize: 15,
+    textDecorationLine: "underline",
+  },
+  scanButton: {
+    backgroundColor: "#2196F3",
+    padding: 10,
+    margin: 10,
+  },
+  buttonText: {
+    fontSize: 15,
+    textAlign: "center",
+    marginTop: 10,
+  },
+  inputView: {
+    flexDirection: "row",
+    margin: 20,
+  },
+  inputBox: {
+    width: 200,
+    height: 40,
+    borderWidth: 1.5,
+    borderRightWidth: 0,
+    fontSize: 20,
+  },
+  scanButton: {
+    backgroundColor: "#66BB6A",
+    width: 50,
+    borderWidth: 1.5,
+    borderLeftWidth: 0,
+  },
+});
